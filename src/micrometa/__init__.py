298806142cc5d2c327baf57fedd2d940143e1117
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-04-08 12:19:06 +0200

    Import submodules by default

--- src/micrometa/__init__.py
+++ src/micrometa/__init__.py
@@ -3,3 +3,9 @@
 # the special string below will be replaced by Maven at build-time, using the
 # project version defined in pom.xml
 __version__ = '${project.version}'
+
+from . import dataset
+from . import experiment
+from . import fluoview
+from . import imagej
+from . import log
